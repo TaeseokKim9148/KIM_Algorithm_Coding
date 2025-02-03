ba1effe8604608c8eaf4a6f4be0b40dd514ddeda
Taeseok Kim <118957691+TaeseokKim9148@users.noreply.github.com>
2025-02-03 14:53:46 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 2684 ms, Memory: 42656 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,17 @@
+n = int(input())
+company = set()  
+
+for _ in range(n):
+    data = input().split()  
+    name = data[0]          
+    states = data[1]        
+
+    if states == "enter":
+        company.add(name)   
+    else:
+        if name in company:
+            company.remove(name)  
+
+result = sorted(company, reverse=True)
+for name in result:
+    print(name)     

0d3acc8b707823abd56208486c7aac3861822656
Taeseok Kim <118957691+TaeseokKim9148@users.noreply.github.com>
2025-03-11 16:56:22 +0900

    [Silver III] Title: N과 M (3), Time: 1100 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,19 @@
+N, M = map(int, input().split())
+
+result = []
+
+def backtrack():
+
+    if len(result) == M:
+        print(' '.join(map(str, result)))
+        return
+    
+    for i in range(1, N+1):
+
+        result.append(i)
+
+        backtrack()
+
+        result.pop()
+
+backtrack() 
